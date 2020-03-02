64027e82346b0a63c71b4b550310992fe085fcfe
Luca Stanger <lstanger@gmx.de>
2020-03-02 15:07:28 +0100

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Luca Stanger on 02.03.20.
+//  Copyright © 2020 Luca Stanger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
