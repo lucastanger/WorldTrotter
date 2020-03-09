519c9063422c673d350ce85f32351a971f537791
Luca Stanger <lstanger@gmx.de>
2020-03-09 14:51:40 +0100

    ??? bei leerem Feld anzeigen

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -15,6 +15,11 @@ class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
     
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
-        celsiusLabel.text = textField.text
+//        celsiusLabel.text = textField.text
+        if let text = textField.text, !text.isEmpty {
+            celsiusLabel.text = text
+        } else {
+            celsiusLabel.text = "???"
+        }
     }
 }
